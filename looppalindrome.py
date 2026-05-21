4da9a191eeeaa1f0856ff3825a130f70f7454437
Sahil Bishnoi <bishnoisahil29736@gmail.com>
2026-05-22 00:16:45 +0530

    Added palindrome project

--- looppalindrome.py
+++ looppalindrome.py
@@ -1,1 +1,12 @@
+import random
 
+num = random.randint(100, 999)
+
+t = str(num)
+
+rev = t[::-1]
+
+if t == rev:
+    print(f"{num} is a palindrome.")
+else:
+    print(f"{num} is not a palindrome.")
